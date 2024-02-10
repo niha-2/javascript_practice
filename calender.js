e095d92b504e86ab3543bd608fc6f2c6d7a477ef
niha-2 <gen.3polym.hn@gmail.com>
2024-02-10 16:12:56 +0900

    [add] JavaScriptでカレンダーを作成する

---
+++ calender.js
@@ -0,0 +1,47 @@
+const daysInMonth = (month, year) => {
+  return new Date(year, month + 1, 0).getDate();
+};
+
+const generateCalendar = (month, year) => {
+  const firstDay = new Date(year, month, 1).getDay();
+  const totalDays = daysInMonth(month, year);
+
+  let calender = "";
+  calender += `       ${month + 1}月 ${year}\n`;
+  calender += " 日 月 火 水 木 金 土\n";
+
+  for (let i = 0; i < firstDay; i++) {
+    calender += "   ";
+  }
+  for (let i = 1; i <= totalDays; i++) {
+    if (i < 10) {
+      calender += " ";
+    }
+    calender += `${i} `;
+    if ((i + firstDay) % 7 === 0) {
+      calender += "\n";
+    }
+  }
+
+  console.log(calender);
+}
+
+// 引数から月と年を取得
+const [, , option, value] = process.argv;
+let month, year;
+if (option === "-m" && value !== undefined) {
+  if (parseInt(value) < 1 || parseInt(value) > 12) {
+    console.log("月は1から12の間で指定してください");
+    process.exit(1);
+  }
+  month = parseInt(value) - 1;
+  year = new Date().getFullYear();
+} else if (option == undefined) {
+  month = new Date().getMonth();
+  year = new Date().getFullYear();
+} else {
+  console.log("Usage: node calender.js [-m month]");
+  process.exit(1);
+}
+
+generateCalendar(month, year);
